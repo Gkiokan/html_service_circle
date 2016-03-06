8bd2f8f5b74f912a530dcb149057592bf2d72075
Gkiokan <gkiokan@hotmail.de>
2016-03-06 21:07:42 +0100

    magic

---
+++ assets/js/service_circle.js
@@ -0,0 +1,16 @@
+/*
+    Project: Service Circle
+    Version: 1.0
+    Author: Gkiokan Sali
+    URI: //gkiokan.net
+    Date: 06.03.2016
+    Comments: -//- 
+
+    
+*/
+
+var service_circle = {
+    
+    
+}
+
